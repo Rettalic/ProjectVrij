741a8885031fe8c63155940fb445bf414ecb6819
Yannick <ybroeks11@gmail.com>
2021-05-14 10:04:55 +0200

    Test playermovement
    Geimporteerde Asset probeersel

---
+++ ProjectVrijV1/Assets/Scripts/CompanionScripts/FollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    private NavMeshAgent companionAgent;
+
+    public GameObject playerobj;
+
+    public float companionDistance;
+
+    private void Start()
+    {
+        companionAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, playerobj.transform.position);
+
+        if(distance < companionDistance)
+        {
+            Vector3 dirToPlayer = transform.position - playerobj.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            companionAgent.SetDestination(newPos);
+        }
+    }
+}
