f46479389c7afe33707cd5513b0eda528808db5a
Yannick <ybroeks11@gmail.com>
2021-06-02 17:43:56 +0200

    EnemySpawner + kansberekening

---
+++ ProjectVrijV1/Assets/Scripts/EnvironmentScripts/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    public GameObject Enemy1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (Random.value <= 0.2)
+            {
+                Instantiate(Enemy1, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.3f, gameObject.transform.position.z), gameObject.transform.rotation);
+            }
+        }
+    }
+
+}
+
