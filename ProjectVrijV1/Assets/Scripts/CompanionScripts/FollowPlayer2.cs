a659a9c7934690530f8652253aafccee3b61a18e
Yannick <ybroeks11@gmail.com>
2021-06-02 17:31:51 +0200

    AI

--- ProjectVrijV1/Assets/Scripts/CompanionScripts/FollowPlayer2.cs
+++ ProjectVrijV1/Assets/Scripts/CompanionScripts/FollowPlayer2.cs
@@ -8,75 +8,97 @@ public class FollowPlayer2 : MonoBehaviour
     public NavMeshAgent agent;
 
     public Transform player;
-
-    /* public int companionHealth;
-
-
-     public LayerMask whatIsGround, whatIsPlayer;
-
-     //patrolling
-     public Vector3 walkPoint;
-     public float walkPointRange;
-
-     //states
-     public float sightRange, followRange;
-     bool playerInsightRange, playerInAttackRange;
-
-     void Start()
-     {
-         agent = gameObject.GetComponent<NavMeshAgent>();
-     }
-
-     private void Awake()
-     {
-         player = GameObject.Find("Player").transform;
-         agent = GetComponent<NavMeshAgent>();
-     }
-
-     private void Update()
-     {
-         playerInsightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-         playerInAttackRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-
-         if (playerInsightRange && !playerInAttackRange) ChasePlayer();
-
-         if (companionHealth <= 0)
-         {
-             Destroy(gameObject);
-
-         }
-     }
-
-     private void ChasePlayer()
-     {
-         agent.SetDestination(player.position);
-
-     }
-
-     void OnTriggerEnter(Collider other)
-     {
-         if (other.CompareTag("PlayerProjectile"))
-         {
-             CompanyTakeDamage(50);
-         }
-     }
-
-     public void CompanyTakeDamage(int companionDamageTake)
-     {
-         companionHealth -= companionDamageTake;
-     }
+    public Transform player2;
 
 
-     private void OnDrawGizmosSelected()
-     {
-         Gizmos.color = Color.red;
-         Gizmos.DrawWireSphere(transform.position, sightRange);
-         Gizmos.color = Color.blue;
-         Gizmos.DrawWireSphere(transform.position, sightRange);
-     } */
+    public int companionHealth;
+    public int companionDamageGive;
+
+    public bool canFollow;
+    public bool isFollowing;
+
+    //patrolling
+    public Vector3 walkPoint;
+    public float walkPointRange;
+
+    //states
+    public float sightRange;
+    bool playerInsightRange;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        canFollow = true;
+        isFollowing = true;
+    }
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     private void Update()
     {
+        StayPut();
+        ComeWithPlayer();
+        playerInsightRange = Physics.CheckSphere(player.transform.position, sightRange);
+
+
+        if (playerInsightRange && canFollow == true) ChasePlayer();
+
+
+        if (companionHealth <= 0)
+        {
+            Destroy(gameObject);
+            GoldPoints.Instance.dropGold(5);
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        StartCoroutine(ChasePlayerNum());
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerProjectile"))
+        {
+            companionTakeDamage(50);
+        }
+    }
+
+    public void companionTakeDamage(int companionDamageTake)
+    {
+        companionHealth -= companionDamageTake;
+    }
+
+    private void StayPut()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && isFollowing == true && Vector3.Distance(player2.transform.position, transform.position) < 4)
+        {
+            isFollowing = false;
+            canFollow = false;
+        }
+    }
+
+    private void ComeWithPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isFollowing == false && Vector3.Distance(player2.transform.position, transform.position) < 4)
+        {
+            isFollowing = true;
+            canFollow = true;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    IEnumerator ChasePlayerNum()
+    {
         agent.SetDestination(player.position);
+        yield return new WaitForSeconds(0.5f);
     }
 }
