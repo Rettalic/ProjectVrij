d7299134a5729b46a3fee4e6f99b5f061cff7265
Yannick <ybroeks11@gmail.com>
2021-06-20 16:51:20 +0200

    Moving

---
+++ ProjectVrijV1/Assets/Scripts/CompanionScripts/DistributeAir.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistributeAir : MonoBehaviour
+{
+    public GameObject companion;
+    public GameObject player;
+
+    public LayerMask whatIsPlayer;
+    public int airRange;
+    public bool range;
+
+    public Slider airAmount;
+    public int airStart;
+    public int airCurrent;
+    public int airSubtract;
+    public int airAdd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        range = Physics.CheckSphere(companion.transform.position, airRange, whatIsPlayer);
+
+        if(range == true)
+        {
+            airCurrent += airAdd;
+
+            if(airCurrent > airStart)
+            {
+                airCurrent = airStart;
+            }
+        }
+        else
+        {
+            airCurrent -= airSubtract;
+        }
+    }
+}
