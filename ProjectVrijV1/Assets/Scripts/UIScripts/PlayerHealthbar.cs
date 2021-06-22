d3382a04c7daa13f26dab9f7583d2e112b0bac3c
WidoBeukhof <widobeukhof@gmail.com>
2021-06-22 15:05:14 +0200

    UI elements added
    Added UI elements to new scene (UI) and prepared it to be added to our characters.

---
+++ ProjectVrijV1/Assets/Scripts/UIScripts/PlayerHealthbar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthbar : MonoBehaviour
+{
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Image image = GetComponent<Image>();
+        
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            image.fillAmount -= .01f;
+        }
+    }
+}
