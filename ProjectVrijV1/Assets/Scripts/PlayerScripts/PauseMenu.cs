75de6d02fbae959fd500893141cb28a74a9eee16
Yannick <ybroeks11@gmail.com>
2021-06-22 21:50:20 +0200

    And I oop SKSKSKS

--- ProjectVrijV1/Assets/Scripts/PlayerScripts/PauseMenu.cs
+++ ProjectVrijV1/Assets/Scripts/PlayerScripts/PauseMenu.cs
@@ -5,80 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenuobj;
-    public bool isPaused;
-    public GameObject Player;
-
-    public GameObject Optionobj;
-    public GameObject MenuObj;
-
-
-    void Awake()
-    {
-        pauseMenuobj.SetActive(false);
-
-    }
 
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-
-            if (isPaused)
-            {
-                ResumeGame();
-            }
-            else {
-                PauseGame();
-            }
+            SceneManager.LoadScene(3);
         }
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(0);
-        }
-
-    }
-
-    public void PauseGame()
-    {
-        pauseMenuobj.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
-        Player.SetActive(false);
-    }
-
-    public void ResumeGame()
-    {
-        pauseMenuobj.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
-        Player.SetActive(true);
-    }
-
-    public void OptionsMenu()
-    {
-        MenuObj.SetActive(false);
-        Optionobj.SetActive(true);
-
-    }
-
-    public void GoBackMenu()
-    {
-        MenuObj.SetActive(true);
-        Optionobj.SetActive(false);
-    }
-
-    public void MainMenuChange()
-    {
-        Time.timeScale = 1f;
-        Debug.Log("te");
-        SceneManager.LoadScene(0);
-    }
-
-    public void change()
-    {
 
     }
 
