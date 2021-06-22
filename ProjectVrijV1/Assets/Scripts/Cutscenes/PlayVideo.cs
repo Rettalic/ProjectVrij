cc1ee6146adf1aedc412d6b1d208d69ba464d6dc
Niceul <nicolelowil17@live.nl>
2021-06-23 01:08:18 +0200

    fk

--- ProjectVrijV1/Assets/Scripts/Cutscenes/PlayVideo.cs
+++ ProjectVrijV1/Assets/Scripts/Cutscenes/PlayVideo.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayVideo : MonoBehaviour
 {
     public GameObject videoPlayer;
     public int timeToStop;
+    public float timeLeft = 20f;
+    public bool video;
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +17,25 @@ public class PlayVideo : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnTriggerEnter (Collider player)
+    void OnTriggerEnter(Collider player)
     {
+        video = true;
+        Debug.Log(timeLeft);
+
         videoPlayer.SetActive(true);
-        Destroy(videoPlayer, timeToStop);
+        //Destroy(videoPlayer, timeToStop);
+
+    }
+    void Update()
+    {
+        if (video == true)
+        {
+            timeLeft -= Time.deltaTime;
+
+            if (timeLeft == 0)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
     }
 }
