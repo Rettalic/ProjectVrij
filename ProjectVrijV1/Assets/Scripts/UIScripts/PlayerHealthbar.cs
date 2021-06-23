c47824734655c7356848e4260fe687261c75b55b
WidoBeukhof <widobeukhof@gmail.com>
2021-06-23 02:01:18 +0200

    WORKING UI WOOOOO

--- ProjectVrijV1/Assets/Scripts/UIScripts/PlayerHealthbar.cs
+++ ProjectVrijV1/Assets/Scripts/UIScripts/PlayerHealthbar.cs
@@ -7,6 +7,13 @@ public class PlayerHealthbar : MonoBehaviour
 {
     private float nextActionTime = 0.0f;
     public float period = 0.1f;
+    public Transform other;
+    public float dist;
+
+    public Image playerOxygenBar;
+    public Image playerHealthBar;
+
+    public FollowPlayer2 companionScript;
 
     public GameObject playerCharacterPortraitDeath;
 
@@ -19,16 +26,31 @@ public class PlayerHealthbar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Image image = GetComponent<Image>();
-        
-        if (Time.time > nextActionTime)
+        if (other)
         {
-            nextActionTime += period;
-            image.fillAmount -= .01f;
+            dist = Vector3.Distance(other.position, transform.position);
+            print("Distance to other: " + dist);
+        }
 
-            if(image.fillAmount < 0.00001) {
-                playerCharacterPortraitDeath.SetActive(true);
-            }
+            Image image = GetComponent<Image>();
+        if (dist > 5)
+        {
+            playerOxygenBar.fillAmount -= .002f;
+        }
+
+        if (dist < 5)
+        {
+            playerOxygenBar.fillAmount += .002f;
+        }
+
+        if (playerOxygenBar.fillAmount > 1)
+        {
+            playerOxygenBar.fillAmount = 1;
+        }
+
+        if (playerOxygenBar.fillAmount < 0.001)
+        {
+            playerHealthBar.fillAmount -= .001f;
         }
     }
 }
